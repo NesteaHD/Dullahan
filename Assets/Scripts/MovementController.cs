90f84ea20c366ed85dbca23977d30a0b62ae445b
Nestor <elnestinatorHD@gmail.com>
2019-10-30 17:33:51 +0100

    Finishing Streets and Adding Buildings
    Added the last part of streets.
    Added a few buildings.

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    //Speeds
+    public float speed = 6.0f;
+    public float turn = 90.0f;
+    public float jump = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 movement = Vector3.zero;
+    public CharacterController moveController;
+    // Update is called once per frame
+    void Update()
+    {
+        //If grounded
+        if (moveController.isGrounded)
+        {
+            //Read vertical data
+            float vertical = Input.GetAxis("Vertical");
+            //Calculate movement based on data
+            movement = new Vector3(0, 0, vertical);
+            //Transform movement to local coordinates
+            movement = transform.TransformDirection(movement);
+            //Multiply by character speed
+            movement *= speed;
+            //Jump
+            if (Input.GetButton("Jump"))
+            {
+                movement.y = jump;
+            }
+        }
+        //If not grounded
+        else
+        {
+            movement.y -= gravity * Time.deltaTime;
+        }
+        transform.Rotate(Input.GetAxis("Horizontal") * Vector3.up * turn * Time.deltaTime);
+        moveController.Move(movement * Time.deltaTime);
+    }
+}
